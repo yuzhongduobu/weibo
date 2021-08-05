c7cb7b9c83a3b853889a945725dff158ec7342d5
yuzhongduobu <1046501164@qq.com>
2021-08-05 22:12:51 +0800

    基础页面

---
+++ resources/views/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title','nice')</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('default')
+
+@section('title','home')
+    
+    
+@endsection
+@section('content')
+  <h1>主页</h1>
+@stop
